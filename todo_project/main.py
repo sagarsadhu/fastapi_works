0e9a82be68c6491d914a10aefb2fea9a402e7e76
sagarsadhu <sagar.sadhu3@gmail.com>
2022-11-02 11:29:10 +0530

    started working on full stack application

--- todo_project/main.py
+++ todo_project/main.py
@@ -3,10 +3,13 @@ import models
 from database import engine
 from routers import auth, todos, users, address
 from company import companyapis, dependencies
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(
     auth.router,
--- todo_project/routers/todos.py
+++ todo_project/routers/todos.py
@@ -3,16 +3,21 @@ import sys
 sys.path.append("..")
 
 from typing import Optional
-from fastapi import Depends, HTTPException, APIRouter
+from fastapi import Depends, HTTPException, APIRouter, Request
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, Field
 from .auth import get_current_user, get_user_exception
 
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 router = APIRouter()
 
 models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
 
 
 def get_db():
@@ -28,6 +33,11 @@ class Todo(BaseModel):
     description: Optional[str]
     priority: int = Field(gt=0, lt=6, description="The priority must be between 1-5")
     complete: bool
+
+
+@router.get("/test")
+async def test(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @router.get("/")
